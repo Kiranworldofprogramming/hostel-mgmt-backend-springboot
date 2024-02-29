9497e639b98f0681e3753753af216125b8d0699c
Kiranworldofprogramming <135489316+Kiranworldofprogramming@users.noreply.github.com>
2024-02-29 14:28:11 +0530

    Update ServletInitializer.java

--- src/main/java/com/nt/ServletInitializer.java
+++ src/main/java/com/nt/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringRestProj09MiniProjectAsLayeredApplicationsApplication.class);
+		return application.sources(SpringRestProj09MiniProjectAsLayeredApplications.class);
 	}
 
 }
